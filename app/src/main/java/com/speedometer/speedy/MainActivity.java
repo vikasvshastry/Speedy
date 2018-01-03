fe18247da2707081c4a2ea2b3a58673bff687dd9
Vikas V Shastry <vikasvshastry@gmail.com>
2018-01-03 21:35:29 +0530

    now can calc distance

--- app/src/main/java/com/speedometer/speedy/MainActivity.java
+++ app/src/main/java/com/speedometer/speedy/MainActivity.java
@@ -1,28 +1,185 @@
 package com.speedometer.speedy;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Formatter;
 import java.util.Locale;
 import android.location.Location;
 import android.location.LocationManager;
 import android.content.Context;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements IBaseGpsListener {
+
+    private static final int PERMISSION_CALLBACK_CONSTANT = 100;
+    private static final int REQUEST_PERMISSION_SETTING = 101;
+    String[] permissionsRequired = new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.INTERNET};
+    private SharedPreferences permissionStatus;
+    private boolean sentToSettings = false;
+
+    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L ;
+
+    Handler handler;
+
+    int Seconds, Minutes, MilliSeconds ;
+
+    TextView textView,dist;
+
+    Boolean started = false;
+
+    double oldLat=0,oldLon=0,totDist=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Button start = findViewById(R.id.start);
+        final Button reset = findViewById(R.id.reset);
+        final Button stop = findViewById(R.id.stop);
+        textView = findViewById(R.id.textView);
+        dist = findViewById(R.id.dist);
+
+        permissionStatus = getSharedPreferences("permissionStatus",MODE_PRIVATE);
+
+        if(ActivityCompat.checkSelfPermission(this, permissionsRequired[0]) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, permissionsRequired[1]) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, permissionsRequired[2]) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,permissionsRequired[0])
+                    || ActivityCompat.shouldShowRequestPermissionRationale(this,permissionsRequired[1])
+                    || ActivityCompat.shouldShowRequestPermissionRationale(this,permissionsRequired[2])){
+                //Show Information about why you need the permission
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Permissions Required");
+                builder.setMessage("This app needs Location permission.");
+                builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        ActivityCompat.requestPermissions(MainActivity.this,permissionsRequired,PERMISSION_CALLBACK_CONSTANT);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            }else {
+                //just request the permission
+                ActivityCompat.requestPermissions(this,permissionsRequired,PERMISSION_CALLBACK_CONSTANT);
+            }
+
+            SharedPreferences.Editor editor = permissionStatus.edit();
+            editor.putBoolean(permissionsRequired[0],true);
+            editor.commit();
+        } else {
+            //You already have the permission, just go ahead.
+        }
+
+        callMe();
+        handler = new Handler();
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StartTime = SystemClock.uptimeMillis();
+                handler.postDelayed(runnable, 0);
+
+                reset.setEnabled(false);
+                started = true;
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TimeBuff += MillisecondTime;
+
+                handler.removeCallbacks(runnable);
+
+                reset.setEnabled(true);
+                started = false;
+
+                oldLat=0;
+                oldLon=0;
+                totDist=0;
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MillisecondTime = 0L ;
+                StartTime = 0L ;
+                TimeBuff = 0L ;
+                UpdateTime = 0L ;
+                Seconds = 0 ;
+                Minutes = 0 ;
+                MilliSeconds = 0 ;
+
+                textView.setText("00:00:00");
+
+                MainActivity.this.updateSpeed(null);
+
+                oldLat=0;
+                oldLon=0;
+                totDist=0;
+                dist.setText("0 km");
+            }
+        });
+
+    }
+
+    public Runnable runnable = new Runnable() {
+
+        public void run() {
+
+            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+
+            UpdateTime = TimeBuff + MillisecondTime;
+
+            Seconds = (int) (UpdateTime / 1000);
+
+            Minutes = Seconds / 60;
+
+            Seconds = Seconds % 60;
+
+            MilliSeconds = (int) (UpdateTime % 1000);
+
+            textView.setText("" + Minutes + ":"
+                    + String.format("%02d", Seconds) + ":"
+                    + String.format("%03d", MilliSeconds));
+
+            handler.postDelayed(this, 0);
+        }
+
+    };
+
+    public void callMe(){
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
             return;
         }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
@@ -38,14 +195,56 @@ public class MainActivity extends AppCompatActivity implements IBaseGpsListener 
         System.exit(0);
     }
 
+    public double GetDistanceFromLatLonInKm(double lat1, double lon1, double lat2, double lon2)
+    {
+        final int R = 6371;
+        // Radius of the earth in km
+        double dLat = deg2rad(lat2 - lat1);
+        // deg2rad below
+        double dLon = deg2rad(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double d = R * c;
+        // Distance in km
+        return d;
+    }
+    private double deg2rad(double deg)
+    {
+        return deg * (Math.PI / 180);
+    }
+
     private void updateSpeed(CLocation location) {
-        // TODO Auto-generated method stub
+        if(started==false)return;
+
         float nCurrentSpeed = 0;
 
         if(location != null)
         {
             location.setUseMetricunits(true);
             nCurrentSpeed = location.getSpeed();
+
+            double lat = location.getLatitude();
+            double lon = location.getLongitude();
+
+            if(!(oldLat==0))
+            {
+                double temp = GetDistanceFromLatLonInKm(oldLat,oldLon,lat,lon);
+                if(temp>0.001)
+                {
+                    totDist+=temp;
+                    Double truncatedDouble = BigDecimal.valueOf(totDist)
+                            .setScale(3, RoundingMode.HALF_UP)
+                            .doubleValue();
+                    dist.setText(truncatedDouble+" km");
+                    oldLat=lat;
+                    oldLon=lon;
+                }
+            }
+            else
+            {
+                oldLat=lat;
+                oldLon=lon;
+            }
         }
 
         Formatter fmt = new Formatter(new StringBuilder());
